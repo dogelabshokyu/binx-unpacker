ff99fbdbd3d8ef534d3a7ae96b3eb5ec85535879
hokyu Kim <hokyu1004@naver.com>
2019-08-27 11:09:12 +0900

    writing code to working on python 3

--- main.py
+++ main.py
@@ -3,4 +3,18 @@
 
 from optparse import OptionParser
 
-parser = OptionParser
+parser = OptionParser()
+parser.add_option("-i", "--input", action="store", type="string", dest="filename", help="input file to parse", default="")
+parser.add_option("-o", "--outdir", action="store", type="string", dest="outdir", help="Output directory", default="./extract")
+parser.add_option("-l", "--list", action="store_true", dest="list", help="List of partitions")
+parser.add_option("-e", "--extract", action="store_true", dest="extract", help="Extract all partitions(without \"-n NAME\")", default = False)
+parser.add_option("-n", "--name", action="store", type="string", dest="name", help="Extract partition by name", default = "")
+(options, args) = parser.parse_args()
+
+if options.filename == "":
+    if str(args) == "[]":
+        print("Use -h to get help")
+    else:
+        options.filename = args[0]
+if options.filename != "":
+    f = open(options.filename, "rb")
