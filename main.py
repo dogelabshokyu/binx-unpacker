8d65a38a92f739c18642ceb62d43b9ead48405e0
hokyu Kim <hokyu1004@naver.com>
2019-09-18 11:58:44 +0900

    update code

--- main.py
+++ main.py
@@ -36,11 +36,8 @@ if options.filename != "":
             f.seek(-4, 1)
             loop = loop + 1
             print("\033[1;36m", loop, "partition count\033[0;0m")
-            print(unpack('2b h 7l 16s', f.read(48)))
-            f.seek(-48, 1)
-            partition = dict(zip(('no1', 'no2', 'id', 'flash', 'start', 'zero', 'size1', 'size2', 'blocksize', 'pagesize', 'none', 'name'), unpack('2b h 7I 16s 48s', f.read(96))))
-            print("\033[1;36m",partition,"\033[0;0m")
-            #partition['name'] = partition['name']
+            partition = dict(zip(('no1', 'no2', 'id', 'flash', 'start', 'zero', 'size1', 'size2', 'blocksize', 'pagesize', 'none', 'name'), unpack('2b h 7I 16s 16s', f.read(64))))
+            f.seek(32, 1)
             partition['no'] = partition['no1']+partition['no2']
             if partition['no2'] != 4:
                 partition['type'] = "MBR " + str(partition['no2'])
@@ -52,11 +49,19 @@ if options.filename != "":
                 partition['flash'] = "Yes"
             if partition['flash'] == 352:
                 partition['flash'] = "No"
+            partition['name'] = partition['name'].decode('ascii')
+            partition['name'] = partition['name'].replace("\x00", "").replace("\x0A", "")
+            print("\033[1;36m",partition,"\033[0;0m")
             #print("\033[0;32m",partition,"\033[0;0m")
             #print("\033[1;95m",partition['name'].decode(),"\033[0;0m")
+            partitions.append(partition)
         else:
             print("\033[1;31mbreak while\n\033[0;0m")
             break
+    print("No.  Name       MBR      Id    Flash  Start         Size           /bytes    Blocksize    Pagesize")
+    for part in partitions:
+        print("%-4i %-10s %-8s 0x%-3X %-6s 0x%08X    0x%08X (%9i)   0x%08X   0x%08X\n" % (part['no'], part['name'], part['type'], part['id'], part['flash'], part['start'], part['size1'], part['size1'], part['blocksize'], part['pagesize']))
+
 '''
             print("\033[1;36m",loop,"partition count\033[0;0m")
             partition_data = unpack('2b h 7I 16s 48s', f.read(96))
