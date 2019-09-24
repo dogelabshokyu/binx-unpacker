458e74b575a50133b4f537cd53f292b3259588e5
hokyu Kim <hokyu1004@naver.com>
2019-09-24 10:38:29 +0900

    update code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 from struct import *
 from optparse import OptionParser
-import binascii
+
 loop = 0
 
 
@@ -13,6 +13,7 @@ parser.add_option("-o", "--outdir", action="store", type="string", dest="outdir"
 parser.add_option("-l", "--list", action="store_true", dest="list", help="List of partitions")
 parser.add_option("-e", "--extract", action="store_true", dest="extract", help="Extract all partitions(without \"-n NAME\")", default = False)
 parser.add_option("-n", "--name", action="store", type="string", dest="name", help="Extract partition by name", default = "")
+parser.add_option("-d", "--debug", action="store_true", dest="debug", help="Turn on Debug Mode")
 (options, args) = parser.parse_args()
 if options.filename == "":
     if str(args) == "[]":
@@ -22,20 +23,21 @@ if options.filename == "":
 if options.filename != "":
     f = open(options.filename, "rb")
     f.seek(-4, 2)
-    print(unpack("I", f.read(4))[0])
+    #print(unpack("I", f.read(4))[0])
     f.seek(-4, 2)
     f.seek(unpack("I", f.read(4))[0])
     f.seek(16, 1)
     partitions = []
     while True:
-        if f.read(4) == "\x0A\x0A\x0A":
+        #print(f.read(4))
+        #f.seek(-4, 1)
+        if f.read(4) == "\x00\x00\x00\x00":
             print("\033[1;31mZero Detected\n\033[0;0m")
             break
-
         if loop < 12:
             f.seek(-4, 1)
             loop = loop + 1
-            print("\033[1;36m", loop, "partition count\033[0;0m")
+            #print("\033[1;36m", loop, "partition count\033[0;0m")
             partition = dict(zip(('no1', 'no2', 'id', 'flash', 'start', 'zero', 'size1', 'size2', 'blocksize', 'pagesize', 'none', 'name'), unpack('2b h 7I 16s 16s', f.read(64))))
             f.seek(32, 1)
             partition['no'] = partition['no1']+partition['no2']
@@ -51,14 +53,17 @@ if options.filename != "":
                 partition['flash'] = "No"
             partition['name'] = partition['name'].decode('ascii')
             partition['name'] = partition['name'].replace("\x00", "").replace("\x0A", "")
-            print("\033[1;36m",partition,"\033[0;0m")
+            #print("\033[1;36m",partition,"\033[0;0m")
             #print("\033[0;32m",partition,"\033[0;0m")
             #print("\033[1;95m",partition['name'].decode(),"\033[0;0m")
             partitions.append(partition)
         else:
             print("\033[1;31mbreak while\n\033[0;0m")
             break
-    print("No.  Name       MBR      Id    Flash  Start         Size           /bytes    Blocksize    Pagesize")
-    for part in partitions:
-        print("%-4i %-10s %-8s 0x%-3X %-6s 0x%08X    0x%08X (%9i)   0x%08X   0x%08X\n" % (part['no'], part['name'], part['type'], part['id'], part['flash'], part['start'], part['size1'], part['size1'], part['blocksize'], part['pagesize']))
+    if options.list:
+        print("No.  Name       MBR      Id    Flash  Start         Size           /bytes    Blocksize    Pagesize")
+        for part in partitions:
+            print("%-4i %-10s %-8s 0x%-3X %-6s 0x%08X    0x%08X (%9i)   0x%08X   0x%08X\n" % (part['no'], part['name'], part['type'], part['id'], part['flash'], part['start'], part['size1'], part['size1'], part['blocksize'], part['pagesize']))
 
+    if options.debug:
+        print("Making Now")
