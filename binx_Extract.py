2ef7ed59d81a3c87226cbc4f6d2ebab50b49d688
HokyuKim <dogelabshokyu@gmail.com>
2019-11-11 12:08:38 +0900

    small edit

--- binx_Extract.py
+++ binx_Extract.py
@@ -1,6 +1,6 @@
 #-*-coding:utf-8-*-
-#python 2.7
-#dont edit code, only translate chinese
+#!/usr/bin/python
+# archive original code
 from struct import *
 from optparse import OptionParser
 import os
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#Edit "binx_Extract.py"
-#python 3.x
+#!/usr/bin/python3
+# Edit "binx_Extract.py"
 
 from struct import *
 from optparse import OptionParser
@@ -11,7 +11,7 @@ parser.add_option("-i", "--input", action="store", type="string", dest="filename
 parser.add_option("-o", "--outdir", action="store", type="string", dest="outdir", help="Output directory", default="./extract")
 parser.add_option("-l", "--list", action="store_true", dest="list", help="List of partitions")
 parser.add_option("-e", "--extract", action="store_true", dest="extract", help="Extract all partitions(without \"-n NAME\")", default = False)
-parser.add_option("-n", "--name", action="store", type="string", dest="name", help="Extract partition by name", default = "")
+parser.add_option("-n", "--name", action="store", type="string", dest="name", help="Extract partition by name", default="")
 parser.add_option("-d", "--debug", action="store_true", dest="debug", help="Turn on Debug Mode")
 parser.add_option("-p", "--partition", action="store_true", dest="partition", help="Extract partition info as JSON file")
 
@@ -33,7 +33,7 @@ if options.filename != "":
         print("Version : ", fw_version)
         f.seek(76, 0)
         fw_build_time = f.read(32).decode().replace("\x00", "")
-        print("Build time : ",fw_build_time)
+        print("Build time : ", fw_build_time)
     f.seek(-4, 2)
     f.seek(unpack("I", f.read(4))[0])
     f.seek(16, 1)
@@ -47,7 +47,7 @@ if options.filename != "":
             partition['type'] = "MBR " + str(partition['no2'])
         if partition['no2'] == 4:
             partition['type'] = "EBR " + str(partition['no1'])
-        if (partition['size1'] != partition['size2']):
+        if partition['size1'] != partition['size2']:
             print("Invalid Partition %s" % partition['name'])
         if partition['flash'] == 340:
             partition['flash'] = "Yes"
@@ -55,7 +55,6 @@ if options.filename != "":
             partition['flash'] = "No"
         partition['name'] = partition['name'].decode('ascii')
         partition['name'] = partition['name'].replace("\x00", "").replace("\x0A", "")
-        #print(partition)
         partitions.append(partition)
         f.seek(4, 1)
         if f.read(4) == b'\x00\x00\x00\x00':
@@ -67,7 +66,7 @@ if options.filename != "":
         print("No.  Name       MBR      Id    Flash  Start         Size           /bytes    Blocksize    Pagesize")
         for part in partitions:
             print("%-4i %-10s %-8s 0x%-3X %-6s 0x%08X    0x%08X (%9i)   0x%08X   0x%08X\n" % (part['no'], part['name'], part['type'], part['id'], part['flash'], part['start'], part['size1'], part['size1'], part['blocksize'], part['pagesize']))
-    if (options.extract) | ((options.name) != ""):
+    if options.extract | ((options.name) != ""):
         for part in partitions:
             if (options.name) != "":
                 if (part['name'].lower() != options.name.lower()):
