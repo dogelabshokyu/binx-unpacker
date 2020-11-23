6ea66844950441b4474b211675a11043783ba9fe
HokyuKim <dogelabshokyu@gmail.com>
2020-11-23 23:54:57 +0900

    small chnage

--- pdl_5_test.py
+++ pdl_5_test.py
@@ -16,8 +16,8 @@ if f.read(0x16).decode() == "$PDL_PHONE_INFO_MAGIC$":
     # check fw info
     print("\x1b[6;30;42mPHONE INFO Detected\x1b[0m")
     f.seek(0x0E, 1)
-    phone_model = f.read(0x10).decode().replace("\x00", "")
-    fw_version = f.read(0x10).decode().replace("\x00", "")
+    MODEL_NAME = f.read(0x10).decode().replace("\x00", "")
+    PANTECH_BUILD_VER = f.read(0x10).decode().replace("\x00", "")
     f.seek(0x08, 1)
     fw_build_date = f.read(0x10).decode().replace("\x00", "")
     fw_build_time = f.read(0x10).decode().replace("\x00", "")
@@ -26,19 +26,23 @@ print("\n\x1b[6;30;42mReading Section info\x1b[0m")
 f.seek(-8, 2)
 f.seek(unpack("I", f.read(0x04))[0], 0)
 # start section head (length = pdl_section_head_length)
-pdl_ver = f.read(0x04).hex()
-pdl_section_head_length = f.read(0x04).hex()
+pdl_ver = unpack("I", f.read(0x04))[0]
+pdl_section_head_length = unpack("I", f.read(0x04))[0]
 pdl_checksum = f.read(0x04).hex()
-pdl_section_index = int.from_bytes(f.read(0x04), byteorder='little')
-pdl_section_struct_size = int.from_bytes(f.read(0x04), byteorder='little')
+pdl_section_index = unpack("I", f.read(0x04))[0] #int.from_bytes(f.read(0x04), byteorder='little')
+pdl_section_struct_size = unpack("I", f.read(0x04))[0] #int.from_bytes(f.read(0x04), byteorder='little')
 f.seek(0x08, 1)
 f.seek(0x08, 1)
 
 
 #####################################
-PDL_INFO = {"PDL Version" : pdl_ver, "Model" : phone_model, "FW Version" : fw_version, "FW Build Date" : fw_build_date,
+PDL_INFO = {"PDL Version" : pdl_ver, "Model" : MODEL_NAME, "FW Version" : PANTECH_BUILD_VER, "FW Build Date" : fw_build_date,
             "FW Build time" : fw_build_time}
 PDL_section_info = {"Section Head Length" : pdl_section_head_length, "All section checksum" : pdl_checksum,
                     "Section Index" : pdl_section_index, "Section struct size" : pdl_section_struct_size}
 PDL = [PDL_INFO, PDL_section_info]
 prnt(PDL)
+'''
+for i in range(pdl_section_index):
+    section =
+'''
